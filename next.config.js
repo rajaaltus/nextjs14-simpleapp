bcf1649213acac45bf882d287e55384e731320d1
rajaaltus <rajaaltus@gmail.com>
2023-10-31 16:27:35 +0700

    fixed auth issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: true,
+  env: {
+    NEXTAUTH_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://nextjs14-tut.vercel.app/"
+        : "http://localhost:3000",
   },
 };
 
